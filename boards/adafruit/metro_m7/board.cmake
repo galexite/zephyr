5f49b7d3831678325633d39243325b08f86c9d2d
George White <me@galexite.uk>
2025-03-23 10:16:28 +0000

    Fix uf2 base offset

--- boards/adafruit/metro_m7/board.cmake
+++ boards/adafruit/metro_m7/board.cmake
@@ -8,6 +8,7 @@
 board_runner_args(pyocd "--target=mimxrt1010")
 board_runner_args(jlink "--device=MIMXRT1011DAE5A")
 board_runner_args(linkserver "--device=MIMXRT1011xxxxx:EVK-MIMXRT1010")
+board_runner_args(uf2 "--board-id=MIMXRT1011-Metro-SD-revA")
 
 if(CONFIG_BUILD_OUTPUT_UF2)
   include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
